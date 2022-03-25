c7fa7834700f6bed5cf4ad73e8815ec2238bca8c
Fusn126 <fushen@utry.cn>
2022-03-25 15:57:36 +0800

    feat: 新增了一个demo项目，学习了一下vue3的父子组件传值方式

---
+++ vue-setup/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="home">
+    <HelloWorld :info="msg" time="42分钟" @myAdd="myAddHander" @myDel='myDelHander' ref="manInfo"></HelloWorld>
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+let msg = ref('公交车-第一次循环')
+const manInfo = ref()
+
+function myAddHander(data){
+  console.log(data);
+  manInfo.value.aging()
+}
+
+function myDelHander(data){
+  console.log(data);
+}
+</script>
+
+<style scoped>
+#home{
+  text-align: center;
+}
+</style>
+
---
+++ vue-setup/src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<!-- 这个是组件 -->
+<template>
+  <div>
+    <h2>你好-我是Loopers</h2>
+    <p>信息:{{ info }}</p>
+    <p>时间：{{ time }}</p>
+    <p>年龄：{{ detailInfo.age }}</p>
+    <p>伴侣：{{ detailInfo.like }}</p>
+    <button @click="handleAdd" class="button">新增</button>
+    <button @click="handleDele" class="button">删除</button>
+  </div>
+</template>
+<script setup>
+import { reactive, ref } from "vue";
+defineProps({
+  info: {
+    type: String,
+    default: '----'
+  },
+  time: {
+    type: String,
+    default: '0分钟'
+  },
+})
+let sex = ref('男')
+let detailInfo = reactive({
+  like: '李诗晴',
+  age: 27
+})
+let $myemit = defineEmits(['myAdd', 'myDel'])
+function handleAdd() {
+  $myemit('myAdd', '新增的数据')
+}
+function handleDele() {
+  $myemit('myDel', '删除的数据')
+}
+function aging() {
+  detailInfo.age++
+
+}
+// 将组件中的属性暴露出去，这样父组件可以获取
+defineExpose({
+  sex,
+  detailInfo,
+  aging
+})
+</script>
+<style scoped>
+.button {
+  margin: 10px;
+}
+</style>
